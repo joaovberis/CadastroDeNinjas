da57d982d33b1043c6b53551555668c8403dc0c0
Batista <batistagod123@gmail.com>
2026-02-05 16:15:46 -0300

    fix: Conserta metodo /listar

--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -2,9 +2,17 @@ package dev.java10x.CadastroDeNinjas.Ninjas;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/ninjas")
 public class NinjaController {
+
+    private NinjaService ninjaService;
+
+    public NinjaController(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
 
     @GetMapping("/boasVindas")
     public String boasVindas () {
@@ -19,10 +27,8 @@ public class NinjaController {
 
     //mostrar todos os ninjas (READ)
     @GetMapping("/listar")
-    public String mostrarTodosOsNinjas () {
-
-        return "Ninjas";
-
+    public List<NinjaModel> listarNinjas () {
+        return ninjaService.listarNinjas();
     }
 
     //mostrar ninja por id (READ)
--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -1,4 +1,24 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class NinjaService {
+
+    private NinjaRepository ninjaRepository;
+
+    public NinjaService(NinjaRepository ninjaRepository) {
+        this.ninjaRepository = ninjaRepository;
+    }
+
+    // LISTAR TODOS OS MEUS NINJAS
+    public List<NinjaModel> listarNinjas() {
+        return ninjaRepository.findAll();
+    }
+
+    public List<NinjaModel> listarNinjasID() {
+        return ninjaRepository.findAllById();
+    }
 }
