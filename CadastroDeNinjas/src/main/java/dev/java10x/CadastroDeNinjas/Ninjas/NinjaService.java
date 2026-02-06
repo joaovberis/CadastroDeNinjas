30311c7b9c7d866e67fc8fdbed3d7158cea9d0ee
Batista <batistagod123@gmail.com>
2026-02-06 15:07:30 -0300

    feat: Inclui a rota de cadastro de ninjas usando metodo POST, tambem inicializa o Postman

--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -24,4 +24,9 @@ public class NinjaService {
         Optional<NinjaModel> ninjaPorId = ninjaRepository.findById(id);
         return ninjaPorId.orElse(null);
     }
+
+    // CRIAR UM NOVO NINJA
+    public NinjaModel criarNinja(NinjaModel ninja) {
+        return ninjaRepository.save(ninja);
+    }
 }
